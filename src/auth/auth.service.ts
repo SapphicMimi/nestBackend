917a72a5de410d7dce2a41815b0f9ccda3c8025d
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2025-01-07 16:24:21 +0100

    Crear registro de Base de Datos

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Model } from 'mongoose';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    console.log(createUserDto);
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel(User.name) 
+    private userModel: Model<User>
+  ) {}
+
+  create(createUserDto: CreateUserDto): Promise<User> {
+
+    try {
+      const newUser = new this.userModel(createUserDto)
+
+      // 1º Encriptar la constraseña
+
+      // 2º Guardar el usuario
+
+      // 3º Generar el JWT
+
+      return newUser.save();
+    } catch (error) {
+      if( error.code == 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exists!`)
+      }
+      throw new InternalServerErrorException('Something terrible happen!!!')
+    }
+
+    
   }
 
   findAll() {
